f969f5a3c8478a6eda966ea7ae166ff22b447f5a
Igor Sviatniy <sviatniy@gmail.com>
2016-02-15 16:51:30 +0200

    Add Tenant model and controller

---
+++ db/migrate/20160215142852_create_tenants.rb
@@ -0,0 +1,14 @@
+class CreateTenants < ActiveRecord::Migration
+  def change
+    create_table :tenants do |t|
+      t.string :type
+      t.string :name
+      t.integer :code
+      t.string :house_number
+      t.integer :apt_number
+      t.text :comment
+
+      t.timestamps null: false
+    end
+  end
+end

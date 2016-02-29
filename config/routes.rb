9b0d89a5481c59e1047eba3cec2e5198aa996d2f
Igor Sviatniy <sviatniy@gmail.com>
2016-02-29 17:05:22 +0200

    Add default resourceful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :tenants, :agreements
+  resources :rent_objects do
+    resources :valuations
+  end
+  namespace :service do
+    resources :sessions, :settlements, :streets
+  end
+  root :to => redirect('/agreements')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

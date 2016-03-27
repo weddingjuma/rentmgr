f4f8c1d58d6bf8fdf2bd48099d0e06ddc86ed96d
Igor Sviatniy <sviatniy@gmail.com>
2016-03-27 21:21:50 +0300

    Add amendments count to stats

--- app/helpers/agreements_helper.rb
+++ app/helpers/agreements_helper.rb
@@ -1,2 +1,27 @@
 module AgreementsHelper
+  def count_changes(agreements)
+    changes = 0
+    if agreements.any?
+      agreements.each do |a|
+          a.extensions.each { |e| changes += 1 if e.change?}
+      end
+      changes
+    else
+      changes
+    end
+    changes
+  end
+
+  def count_extensions(agreements, category)
+    extensions = 0
+    if agreements.any?
+      agreements.each do |a|
+        a.extensions.each { |e| extensions += 1 if e.category == category}
+      end
+      extensions
+    else
+      extensions
+    end
+    extensions
+  end
 end

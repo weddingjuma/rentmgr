d60eeb5d6edc7b721d4d3a67584ec0c78ee3cf0d
Igor Sviatniy <sviatniy@gmail.com>
2016-04-29 23:33:27 +0300

    Add restrictions to Street and Settlement associations

--- app/models/street.rb
+++ app/models/street.rb
@@ -1,6 +1,6 @@
 class Street < ActiveRecord::Base
-  has_many :tenants
-  has_many :rent_objects
+  has_many :tenants, dependent: :restrict_with_error
+  has_many :rent_objects, dependent: :restrict_with_error
   enum kind: %w(str blvd lane)
 
   validates :name, :kind, presence: true
